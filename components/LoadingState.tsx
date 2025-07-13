aebf4b3790884678269c2c97b0ab480a83a3706c
Haider Alshamma <haideralsh@gmail.com>
2025-07-12 23:53:50 -0400

    Adjust the LoadingState

--- components/LoadingState.tsx
+++ components/LoadingState.tsx
@@ -1,16 +1,19 @@
 export function LoadingState() {
   return (
-    <div className="flex flex-col space-y-4 animate-pulse">
-      <div className="h-4 bg-[#EAE7EC] rounded w-48"></div>
-      {Array.from({ length: 6 }).map((_, i) => (
+    <div className="flex flex-col gap-4 animate-pulse">
+      <div className="h-4 bg-[#EAE7EC] mb-2 rounded w-48"></div>
+      {Array.from({ length: 10 }).map((_, i) => (
         <div
           key={i}
-          className="flex justify-between items-center py-3 border-b border-[#EAE7EC]"
+          className="flex justify-between items-center pb-4 border-b last:border-b-0 border-[#EAE7EC]"
         >
-          <div className="h-5 bg-[#EAE7EC] rounded w-32"></div>
+          <div
+            className={`h-5 bg-[#EAE7EC] rounded ${i % 2 === 0 ? "w-32" : "w-40"}`}
+          ></div>
           <div className="flex gap-1.5">
-            <div className="h-5 w-5 bg-[#EAE7EC] rounded"></div>
-            <div className="h-5 w-5 bg-[#EAE7EC] rounded"></div>
+            <div className="h-6 w-8 bg-[#EAE7EC] rounded"></div>
+            <div className="h-6 w-6 bg-[#EAE7EC] rounded"></div>
+            <div className="h-6 w-6 bg-[#EAE7EC] rounded"></div>
           </div>
         </div>
       ))}
